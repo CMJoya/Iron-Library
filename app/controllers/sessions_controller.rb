7df3145b8b15ddd28db5116d9c45f6626ab9cedf
Cris Joya <MJoya@mac.attlocal.net>
2016-03-31 01:43:05 -0500

    Add To Views, And Controllers, Styled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      flash.now[:alert] = "Something Is Wrong With Your Email Or Password"
       render :new
     end
   end
